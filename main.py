86f727d2db7483c1f3a676c27e171527336f1d35
JESUS CORDOVA <cordovangohgster@gmail.com>
2022-08-27 20:29:05 -0500

    validation_body

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-#Python
-from typing import Optional
+# Python
+from typing import Optional # Sirve para definir tipos de datos en python y volver a python como un lenguaje de programación estatico
 
 #Pydantic
-from pydantic import BaseModel
+from pydantic import BaseModel # Sirve para definir los modelos de datos que se usaran en la aplicacion
 
-#FastAPI
-from fastapi import FastAPI
-from fastapi import Body, Query
+# FastAPI
+from fastapi import FastAPI  # Sirve para definir la API de la aplicacion
+from fastapi import Body, Query, Path # Sirve para definir el tipo de dato que se usara en la API
+
 
 app = FastAPI()
 
 #models
+class Location(BaseModel):
+    city : str
+    state: str
+    country:str
+
 
 class Person(BaseModel):
     first_name: str
@@ -55,3 +61,30 @@ def show_person(
     person_id: int = '..., gt=0'
 ):
     return {person_id : "it exits"}
+
+
+# Validations: Request body parameters
+@app.put("/person/{person_id}")
+def show_person(
+    person_id: int = '..., gt=0, title="Person id", description="then person id"',
+    person: Person = Body(...),
+    location: Location = Body(...)
+):
+    #Varias manera de concatenar dependiendo necesidad
+    #result = Person.dic() #En version anterior que ya no funciona en esta
+    #result.update(Location.dic())
+    
+    #result = dict(person) #junta todo en uno solo sin nodos
+    #result.update(dict(location))
+    #return result
+    
+    #return  {**dict(person),**dict(location)} #junta todo sin nodos como el anterior
+
+    #return { #junta todo con los dos nodos por separado
+    #    "person": person,
+    #    "location": location
+    #}
+
+    return person.dict() | location.dict() #junta todo también
+
+    
